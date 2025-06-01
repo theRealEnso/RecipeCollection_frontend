d2a26a64e4a5a19b0e18e1640655d479360f9a49
Ben <bennnnnnnvu@gmail.com>
2025-05-31 17:57:08 -0700

    create basic expo app

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        // headerShown: false,
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+          },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => <Ionicons size={28} name={focused ? "information-circle" : "information-circle-outline"} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
